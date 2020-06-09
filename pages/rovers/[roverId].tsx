90affac466df98fb04c9ef33333f4c70d6379fa4
Koshin <koshin@sibipro.com>
2020-06-09 11:34:53 -0700

    Added RoverDetail page, it was broken.

--- pages/rovers/[roverId].tsx
+++ pages/rovers/[roverId].tsx
@@ -1,5 +1,11 @@
 import { useRouter } from "next/router";
 
-const RoverDetail = () => {};
+const RoverDetail = () => {
+  return (
+    <div>
+      <h1>Rover Detail</h1>
+    </div>
+  );
+};
 
 export default RoverDetail;
